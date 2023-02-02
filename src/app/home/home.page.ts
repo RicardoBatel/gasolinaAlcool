b4593241f6e1e5fa673583d3c400dfc7a682e29a
RicardoBatel <ricardo.batel@hotmail.com>
2023-02-02 11:09:54 -0300

    App finalizado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,36 @@ export class HomePage {
 
   constructor() {}
 
+  public resultado: String = "Resultado"
+  precoAlcool = ""
+  precoGasolina = ""
+
+  calcular(){
+
+    //validar so os campos foram preenchidos
+    if(this.precoAlcool && this.precoGasolina){
+
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+      /*
+      faz cálculo (precoAlcool / precoGasolina)
+      se: resultado >= 0.7 melhor utilizar gasolina
+      senão: melhor álcoll
+      */
+
+      var res = pAlcool / pGasolina
+
+      if(res >= 0.7){
+        this.resultado = "Melhor utilizar Gasolina"
+      }else{
+        this.resultado = "Melhor utilizar Álcool"
+      }
+      
+    }else{
+      this.resultado = "Preencha corretamento os campos!"
+    }
+
+  }
+
 }
